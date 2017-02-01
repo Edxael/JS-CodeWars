a3b6ea6996592e6fd62dce774a03450e3aa3fa48
Edxael <zerphyr.r@gmail.com>
2017-02-01 12:48:21 -0700

    Kata:07-The_Office_II_-_Boredom_Score.js

--- 07-The_Office_II_-_Boredom_Score.js
+++ 07-The_Office_II_-_Boredom_Score.js
@@ -1,11 +1,32 @@
-// 06-Calculate-Average
+// 07-The_Office_II_-_Boredom_Score.js
 // From codewars.com by Edxael  (that’s me)
-// https://www.codewars.com/kata/calculate-average/javascript
+// https://www.codewars.com/kata/the-office-ii-boredom-score/javascript
 
 // ------------------------------------------------------------------------------------------------------------------------
 //   Description:
 //
-//  Write function avg which calaculates average of numbers in given list.
+//  Every now and then people in the office moves teams or departments. Depending what people are doing with their time they can become more or less boring. Time to assess the current team.
+//
+//   You will be provided with an object(staff) containing the staff names as keys, and the department they work in as values.
+//
+//   Each department has a different boredom assessment score, as follows:
+//
+//   accounts = 1
+//   finance = 2
+//   canteen = 10
+//   regulation = 3
+//   trading = 6
+//   change = 6
+//   IS = 8
+//   retail = 5
+//   cleaning = 4
+//   pissing about = 25
+//
+//   Depending on the cumulative score of the team, return the appropriate sentiment:
+//
+//   <=80: 'kill me now'
+//   < 100 & > 80: 'i can handle this'
+//   100 or over: 'party time!!'
 // ------------------------------------------------------------------------------------------------------------------------
 
 function boredom(staff){
@@ -41,32 +62,30 @@ var staff = {tim: 'change', jim: 'accounts',
 // ----------------------------------------------------
 //       Run Output
 //
-//  5
+//  kill me now
 // ----------------------------------------------------
 
 
-// Note:  The following code is not written by me, it was created by another user of codewars.com it belongs to the user: danielsnewman
+// Note:  The following code is not written by me, it was created by another user of codewars.com it belongs to the user: WompWomp
 
-// function find_average(array) {
-//   var sum = array.reduce((a, b) => a + b, 0);
-//   return sum/array.length;
-// }
-// find_average(array)
+function boredom(staff){
+   var map = {
+     accounts:1,
+     finance:2,
+     canteen:10,
+     regulation:3,
+     trading: 6,
+     change:6,
+     IS:8,
+     retail:5,
+     cleaning:4,
+     'pissing about':25
+   };
 
+   var score = Object.keys(staff).reduce(
+     function(a,b){
+       return a+map[staff[b]]
+    },0);
 
-
-
-// {tim: 'change', jim: 'accounts',
-//   randy: 'canteen', sandy: 'change', andy: 'change', katie: 'IS',
-//   laura: 'change', saajid: 'IS', alex: 'trading', john: 'accounts',
-//   mr: 'finance' }
-
-//   {tim: 'IS', jim: 'finance',
-//   randy: 'pissing about', sandy: 'cleaning', andy: 'cleaning',
-//   katie: 'cleaning', laura: 'pissing about', saajid: 'regulation',
-//   alex: 'regulation', john: 'accounts', mr: 'canteen' }
-
-//   { tim: 'accounts', jim: 'accounts',
-//   randy: 'pissing about', sandy: 'finance', andy: 'change',
-//   katie: 'IS', laura: 'IS', saajid: 'canteen', alex: 'pissing about',
-//   john: 'retail', mr: 'pissing about' }
+   return score <= 80 ? 'kill me now': score < 100 && score > 80 ? 'i can handle this' : 'party time!!';
+}
