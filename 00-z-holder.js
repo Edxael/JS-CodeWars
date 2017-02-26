91eea2c057130c1b14f2c53a910c7b326f17cbd2
Edxael <zerphyr.r@gmail.com>
2017-02-26 14:52:32 -0700

    Kata: 16-Los-Angeles-score.js

--- 00-z-holder.js
+++ 00-z-holder.js
@@ -2,23 +2,61 @@
 console.log("This is a Holder file");
 // ---------------------------------
 
-console.log("Tetona");
-console.log("");
 
-function makeParts(arr, chunkSize) {
-    console.log(arr);
-    console.log(chunkSize);
+function getLosAngelesPoints(results) {
+    var arr = [];
+    var total = 0;
+
+
+
+
+        for(var x = 0; x < results.length; x++){
+            console.log(results[x][0]);
+            var str = results[x][0].split("");
+            function hasNumbers(str){return /\d/.test(str);};
+            str = hasNumbers(str);
+
+            console.log(str);
+            console.log(" ");
+
+          if( (results[x][0].startsWith("Los Angeles "))  && ( !str ) ){
+              arr = results[x][1].split(":");
+                    console.log(results[x][0]);
+                    console.log(parseInt(arr[0]));
+              total += parseInt(arr[0]);
+              console.log(total);
+              console.log("");
+          }
+
+        }
     console.log(" ");
+    return total;
+}
 
-    var times = Math.ceil(arr.length / chunkSize);
-    console.log("will run: " + times);
-    var holder = [];
-    console.log("resultados:");
 
-    for(var x=0; x<times; x++){
-        console.log("Inside round: " + (x+1));
-        holder.push(  arr.splice(0,2)  );
-    }
-    console.log(holder);
-    return holder;
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------
+function getLosAngelesPoints(results) {
+    var arr = [];
+    var total = 0;
+
+        for(var x = 0; x < results.length; x++){
+
+          if(results[x][0].startsWith("Los Angeles ")){
+              arr = results[x][1].split(":");
+              total += parseInt(arr[0]);
+          }
+
+        }
+
+    return total;
 }
