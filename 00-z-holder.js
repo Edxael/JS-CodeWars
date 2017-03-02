a35de5519793f67ec53597d15e0afff7c22deffa
Edxael <zerphyr.r@gmail.com>
2017-03-02 11:16:36 -0700

    Kata: 20-Character-limits.js

--- 00-z-holder.js
+++ 00-z-holder.js
@@ -3,6 +3,22 @@ console.log("This is a Holder file");
 // ---------------------------------
 
 
-for(var x = 0; x < arr.length; x++){
-    if(arr[x].length > arr[0].length){ delete arr[x]; }
+
+var count = 0;
+var locat = 0;
+arrH = []
+arrO = text.split("");
+
+while(count <= max){
+
+    if(arrO[locat] == " "){
+        arrH.push(arrO[locat]);
+        locat += 1;
+    }else{
+        arrH.push(arrO[locat]);
+        locat += 1;
+        count += 1;
+    }
+
 }
+text = arrH.join("");
