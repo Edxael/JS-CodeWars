e71645c2e611a19419283ebaaae6c6b33a4c5aa2
Edxael <zerphyr.r@gmail.com>
2017-03-07 12:35:34 -0700

    Kata: 21-Get-the-Middle-Character.js

--- 00-z-holder.js
+++ 00-z-holder.js
@@ -2,26 +2,21 @@
 console.log("This is a Holder file");
 // ---------------------------------
 
-function reverseLetter(str) {
-    console.log(str);
 
-    var arr = str.split("");
-    console.log(arr);
+function getMiddle(s){
 
-    for(var x = 0; x < arr.length; x++){
-        if( arr[x].match( /[a-zA-Z]/ ) ){
-            console.log("Yes: " + arr[x]);
-
-        }else{
-            console.log("Noooooooo: " + arr[x]);
-            delete arr[x];
-        }
+    if(s.length % 2 === 0){
+        // console.log(s);
+        // console.log(s.length);
+        // console.log(s.length / 2);
+        console.log( s[((s.length / 2)-1)] + s[(s.length / 2)] );
+    }else{
+        // console.log("Pos Noo");
+        // console.log(s);
+        // console.log(s.length);
+        // console.log(Math.floor(s.length / 2));
+        console.log(s[Math.floor(s.length / 2)]);
     }
 
-    console.log(" ");
-    arr = arr.filter(function(n){return n != undefined}).reverse();
-    console.log(arr);
-
-    return str = arr.join("");
 
 }
